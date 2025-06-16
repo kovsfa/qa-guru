3edcb180faa337ce5dd2ad63742424d6545a4361
Sofia Kovaleva <124074895+kovsfa@users.noreply.github.com>
2025-06-16 19:38:37 +0500

    Update Main.java

--- src/main/java/kov/sfa/Main.java
+++ src/main/java/kov/sfa/Main.java
@@ -2,8 +2,8 @@ package kov.sfa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome QA GURU!");
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Hello and welcome QA GURU!!!");
+        for (int i = 10; i <= 1; i--) {
             System.out.println("i = " + i);
         }
     }
