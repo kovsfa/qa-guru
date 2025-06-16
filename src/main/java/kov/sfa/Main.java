bb00306d641a70f172a73b8267ab1a074891b5c4
Sofia Kovaleva <kov.sfa@gmail.com>
2025-06-16 19:35:14 +0500

    fix bugs for testers

--- src/main/java/kov/sfa/Main.java
+++ src/main/java/kov/sfa/Main.java
@@ -1,8 +1,8 @@
 package kov.sfa;
 
 public class Main {
-    public static String msg = "Hello and welcome QA GURU!"
-    public static void main(String[] args)u {
+    public static String msg = "Hello and welcome QA GURU!";
+    public static void main(String[] args) {
         System.out.print(msg);
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
